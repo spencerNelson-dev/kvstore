894ad12c3e1676b0f5f86a5b58fb77ef6fd592c3
Spencer Nelson <spencernelson144@gmail.com>
2021-06-29 11:15:08 -0600

    finished tutorial

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,19 @@ fn main() {
     let key = args.next().expect("key was not there");
     let value = args.next().unwrap();
     println!("The key is '{}' and the value is '{}'", key, value);
-    let contents = format!("{}\t{}\n", key, value);
-    std::fs::write("kv.db", contents).unwrap();
 
-    let database = Database::new().expect("Creating db failed.");
-
+    let mut database = Database::new().expect("Creating db failed.");
+    database.insert(key.to_uppercase(), value.clone());
+    database.insert(key, value);
+    match database.flush() {
+        Ok(()) => println!("Saved!"),
+        Err(err) => println!("Error : {}", err),
+    }
 }
 
 struct Database {
     map: HashMap<String, String>,
+    flush: bool,
 } 
 
 impl Database {
@@ -24,6 +28,35 @@ impl Database {
             let (key, value) = line.split_once('\t').expect("Corrupt database");
             map.insert(key.to_owned(), value.to_owned());
         }
-        Ok(Database{map: map})
+        Ok(Database { map, flush: false })
     }
+
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.flush = true;
+        do_flush(&self)
+    }
+}
+
+impl Drop for Database {
+    fn drop(&mut self) {
+        if !self.flush{
+        let _ = do_flush(self);
+        }
+
+    }
+}
+
+fn do_flush(database: &Database) -> std::io::Result<()> {
+        let mut contents = String::new();
+        for (key, value) in &database.map {
+            contents.push_str(key);
+            contents.push('\t');
+            contents.push_str(value);
+            contents.push('\n');
+        }
+        std::fs::write("kv.db", contents)
 }
